d62ab7d0af6555dc096fdccad24df6b73d30eb21
TainaMalave <Taina.Malave@gmail.com>
2020-03-11 16:18:11 -0400

    temp database

--- python/tempLog.py
+++ python/tempLog.py
@@ -1,8 +1,12 @@
+# -*- coding: utf-8 -*-
 #Import Libraries we will be using
 import RPi.GPIO as GPIO
 import Adafruit_DHT
 import time
+import datetime
 import os
+import sqlite3
+
 #Assign GPIO pins
 redPin = 27
 tempPin = 17
@@ -16,6 +20,14 @@ blinkDur = .3
 #Number of times to Blink the LED
 blinkTime = 7
 #---------------------------------------------------------------------
+
+# connection to DB and making a cusor object so that i can access 
+conn = sqlite3.connect('temperature.db')
+c = conn.cursor()
+
+c.execute('''CREATE TABLE IF NOT EXISTS temperature
+             (Date text, Temperature int, Humidity int)''')
+
 #Initialize the GPIO
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(redPin,GPIO.OUT)
@@ -32,21 +44,26 @@ def readF(tempPin):
     temperature = temperature * 9/5.0 +32
     if humidity is not None and temperature is not None:
         tempFahr = '{0:0.1f}*F'.format(temperature)
+        hum = '{0:0.1f}%'.format(humidity)
     else:
         print('Error Reading Sensor')
-    return tempFahr
+    return tempFahr, hum
 
 try:
-	with open("../log/tempLog.csv", "a") as log:
-		while True:
-			input_state = GPIO.input(buttonPin)
-			if input_state == True:
-				for i in range (blinkTime):
-					oneBlink(redPin)
-				time.sleep(.2)
-				data = readF(tempPin)
-				print (data)
-				log.write("{0},{1}\n".format(time.strftime("%Y‐%m‐%d %H:%M:%S"),str(data)))
+    starttime=time.time()
+    while True:
+        input_state = GPIO.input(buttonPin)
+        if input_state == True:
+            for i in range (blinkTime):
+                oneBlink(redPin)
+            time.sleep(.2)
+            tempFahr, hum = readF(tempPin)
+            date = datetime.datetime.now()
+            print(tempFahr ,hum)
+            params = (date, tempFahr, hum)
+            c.execute('INSERT INTO temperature VALUES (?, ?, ?)',(params))
+            conn.commit()
+            time.sleep(60.0 - ((time.time() - starttime) % 60.0))
 
 except KeyboardInterrupt:
     os.system('clear')
